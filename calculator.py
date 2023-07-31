12036713c2ca675df457d8d85c7d89c47d01105e
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-07-31 20:59:33 -0300

    create calculator interface

---
+++ calculator.py
@@ -0,0 +1,75 @@
+from tkinter import *
+
+
+def button_press(num):
+    pass
+
+
+def result():
+    pass
+
+
+def reset():
+    pass
+
+
+window = Tk()
+window.title('Calculator')
+window.geometry('500x500')
+
+display_text = ""
+
+display_label = StringVar()
+
+label = Label(window, textvariable=display_label, font=('Arial', 20), bg='white', width=24, height=2, pady=5)
+label.pack(pady=10)
+
+frame = Frame(window)
+frame.pack()
+
+button1 = Button(frame, text=1, width=9, height=4, font=35, command=lambda: button_press(1))
+button1.grid(row=0, column=0)
+
+button2 = Button(frame, text=2, width=9, height=4, font=35, command=lambda: button_press(2))
+button2.grid(row=0, column=1)
+
+button3 = Button(frame, text=3, width=9, height=4, font=35, command=lambda: button_press(3))
+button3.grid(row=0, column=2)
+
+button4 = Button(frame, text=4, width=9, height=4, font=35, command=lambda: button_press(4))
+button4.grid(row=1, column=0)
+
+button5 = Button(frame, text=5, width=9, height=4, font=35, command=lambda: button_press(5))
+button5.grid(row=1, column=1)
+
+button6 = Button(frame, text=6, width=9, height=4, font=35, command=lambda: button_press(6))
+button6.grid(row=1, column=2)
+
+button7 = Button(frame, text=7, width=9, height=4, font=35, command=lambda: button_press(7))
+button7.grid(row=2, column=0)
+
+button8 = Button(frame, text=8, width=9, height=4, font=35, command=lambda: button_press(8))
+button8.grid(row=2, column=1)
+
+button9 = Button(frame, text=9, width=9, height=4, font=35, command=lambda: button_press(9))
+button9.grid(row=2, column=2)
+
+button0 = Button(frame, text=0, width=9, height=4, font=35, command=lambda: button_press(0))
+button0.grid(row=3, column=0)
+
+plus_button = Button(frame, text='+', width=9, height=4, font=35, command=lambda: button_press(1))
+plus_button.grid(row=0, column=3)
+
+subtraction_button = Button(frame, text='-', width=9, height=4, font=35, command=lambda: button_press())
+subtraction_button.grid(row=1, column=3)
+
+multiplication_button = Button(frame, text='*', width=9, height=4, font=35, command=lambda: button_press(1))
+multiplication_button.grid(row=2, column=3)
+
+division_button = Button(frame, text='/', width=9, height=4, font=35, command=lambda: button_press(1))
+division_button.grid(row=3, column=3)
+
+equal_button = Button(frame, text='=',  width=19, height=4, font=35, command=lambda: equal_button())
+equal_button.grid(row=3, column=1, columnspan=2)
+
+window.mainloop()
